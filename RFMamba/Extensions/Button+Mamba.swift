75746a965423846812ebd428c0c48fe41fe7b77c
Frank Huang <ijamhh@gmail.com>
2018-06-16 20:28:20 +0800

    Add Button+Mamba extension file

---
+++ RFMamba/Extensions/Button+Mamba.swift
@@ -0,0 +1,19 @@
+//
+//  Button+Mamba.swift
+//  RFMamba
+//
+//  Created by Frank on 2018/6/16.
+//  Copyright © 2018年 RFMamba. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension Mamba where Base: UIButton {
+    
+    public func setImage(with resource: Resource?,
+                         for state: UIControlState,
+                         placeholder: UIImage? = nil) {
+        
+    }
+}
