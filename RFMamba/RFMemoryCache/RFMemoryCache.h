9afdfa395d5d51d808281ad444a026c60273af8b
roy <roy.cao@farfetch.com>
2018-06-16 18:24:45 +0800

    Use OC write RFMemoryCache

---
+++ RFMamba/RFMemoryCache/RFMemoryCache.h
@@ -0,0 +1,40 @@
+//
+//  RFMemoryCache.h
+//  RFMamba
+//
+//  Created by roy.cao on 2018/6/16.
+//  Copyright © 2018 roy.cao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RFMemoryCache : NSObject
+
+@property (nonatomic, assign) NSUInteger maxLengthForFIFO;
+
+@property (nonatomic, assign) NSUInteger maxLengthForLRU;
+
+@property (nonatomic, assign) NSUInteger cacheSizeLimit;
+
++ (instancetype)defaultCache;
+
+- (BOOL)hasCacheWithURL:(NSString *)URL;
+
+- (void)cacheImage:(UIImage *)image URL:(NSString *)URL;
+
+- (UIImage * _Nullable)imageWithURL:(NSString *)URL;
+
+- (void)clearWithURL:(NSString *)URL;
+
+- (void)clear;
+
+- (int64_t)cacheSize;
+
+- (int64_t)cacheSize:(BOOL)accurate;
+
+@end
+
+NS_ASSUME_NONNULL_END
--- RFMamba/ViewController.swift
+++ RFMamba/ViewController.swift
@@ -15,7 +15,18 @@ class ViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        let url:URL = URL(string: "https://blog.ibireme.com/wp-content/uploads/2015/05/RunLoop_0.png")!
+        
+        let data = try? Data(contentsOf: url )
+        
+        let image = UIImageView.init(frame: CGRect(x: 100, y: 100, width: 200, height: 200))
+        image.image = UIImage(data: data!)
+        self.view.addSubview(image)
+        
+        RFMemoryCache.default().cacheImage(image.image!, url: "http://image.baidu.com")
+        
+        let a = RFMemoryCache.default().image(withURL: "http://image.baidu.com")
+        print(a)
     }
 
 
